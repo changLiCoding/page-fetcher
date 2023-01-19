90bc2d2b8932a6bb995185bed2f003cb9b9a9bfb
javascriptsucks <evix1988@live.cn>
2023-01-19 12:18:15 -0600

    check-file-name-validation

--- fetcher.js
+++ fetcher.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const fs = require('fs');
+const path = require('path');
 
 const readline = require('readline');
 
@@ -8,39 +9,43 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+const fileWriteAsync = (data,file) => {
+  fs.writeFile(`./${file}`, data, err => {
+    if (err) {
+      console.error(err);
+    }
+    if (data) {
+      const string = data;
+      console.log(`Downloaded and saved ${string.length} bytes to ${file}`);
+    }
+    // file written successfully
+    process.exit();
+  });
+};
+
 const fetcher = (url,file) => {
 
   request(url,(error,response,body) => {
     if (error) throw new Error(error);
-
-    fs.readFile(file,'utf-8',(err,data) => {
-      if (err) throw new Error(error);
+    if (!file === path.basename(file)) throw new Error('Not valid file name. ');
+    fs.readFile(`./${file}`,'utf-8',(err,data) => {
+      if (err) {
+        fileWriteAsync(body, file);
+      }
       if (data) {
         rl.question('Same file name detected, do you still want to continue over written the file? (Y/N) y for yes n for skip it. ',(key) => {
           if (key === 'y') {
-            fs.writeFile(`${file}`, body, err => {
-              if (err) {
-                console.error(err);
-              }
-              if (body) {
-                const string = body;
-                console.log(`Downloaded and saved ${string.length} bytes to ${file}`);
-              }
-              // file written successfully
-            });
+            fileWriteAsync(body,file);
+            console.log('Over written accomplished. ');
           } else {
+            console.log("Over written canceled.");
             process.exit();
           }
         });
       }
     });
 
-
-    //console.log('error:', error);
-    //console.log('statusCode:', response && response.statusCode);
-    //console.log('body:', body.body);
   });
-
 };
 const argv = process.argv.slice(2);
 console.log(argv);
